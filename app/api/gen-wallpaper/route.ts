8ecd07743c8d69521dd8f0c76b3a495f90388a33
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-06-01 00:04:26 +0800

    debug proxy

--- app/api/gen-wallpaper/route.ts
+++ app/api/gen-wallpaper/route.ts
@@ -1,21 +1,34 @@
 import { NextResponse } from "next/server";
 import { getOpenAIClinet } from "@/utils/openai";
 
+var colors = require("colors");
+
 export async function POST(req: Request) {
   const client = getOpenAIClinet();
-  console.log("OpenAI client initialized");
+  console.log("OpenAI client initialized".green);
   try {
     const request = await req.json();
     console.log("Request received: ", request);
     const desription = request.description;
-    const response = await client.images.generate({
+    
+    const requestBody = {
       prompt: `A beautiful wallpaper with the description: ${desription}`,
-      model: "dall-e-2",
-      size: "1024x1024",
+      model: "dall-e-3",
+      size: "1792x1024",
       n: 1,
       quality: "hd",
       response_format: "url",
-    });
+    };
+    console.log("Request body: ", requestBody);
+    
+    const myIP = await fetch("https://api.ipify.org?format=text");
+    console.log(`myIP: ${await myIP.text()}`.red);
+
+    const response = await client.images.generate(requestBody);
+    console.log(
+      "successfully generated image"
+    );
+
     return NextResponse.json({
       status: 200,
       message: "success",
@@ -23,6 +36,10 @@ export async function POST(req: Request) {
         img_url: response.data[0].url,
       },
     });
+
+    console.log(
+      `response: ${response.data[0].url}`.green
+    )
   } catch (error) {
 		return NextResponse.json({
 			status: 500,
