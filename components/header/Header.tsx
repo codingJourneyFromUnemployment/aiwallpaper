057d13ec8cdd93b794021e24699e7141d9e77a09
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-06-03 05:50:26 +0000

    adding navbar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,8 @@
-import NavBar from './NavBar';
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <header>
+    <header className="sticky top-0 z-50 bg-white shadow-lg w-full flex justify-center">
       <NavBar />
     </header>
   );
--- components/header/NavBar.tsx
+++ components/header/NavBar.tsx
@@ -1,81 +1,24 @@
-
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a href="/">Homepage</a>
-            </li>
-            <li>
-              <a href="/portfolio">Portfolio</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-          </ul>
-        </div>
+    <div className="navbar bg-base-100 flex my-3 mx-3 items-center w-full justify-between text-xl md:mx-6 md:text-2xl xl:mx-10 xl:text-3xl">
+      <div className="">
+        <a className="btn btn-ghost text-primary">PixiAI</a>
       </div>
-      <div className="navbar-center">
-        <a href="/" className="btn btn-ghost text-xl">
-          daisyUI
-        </a>
-      </div>
-      <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
+      <div className="">
+        <button className="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
             fill="none"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            className="inline-block w-5 h-5 stroke-current"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
           </svg>
-        </button>
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
         </button>
       </div>
     </div>
